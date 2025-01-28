dba2ac2b1f23259d27b8021216a6906e3b9fd69a
artem-Tolmachev <89ufisum@gmail.com>
2025-01-28 11:57:54 +0100

    add pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,37 @@
+import styles from './styles.module.css';
+
+const Pagination = ({
+    totalPages, 
+    handleNextPage, 
+    handlePrevtPage, 
+    handlePageClick, 
+    currentPage}) => {
+   
+    return(
+        <div className={styles.pagination}>
+           <button
+            className={styles.arrow}
+            onClick={handlePrevtPage}
+            disabled={currentPage <= 1}
+            >{'<'}</button>
+           <div className={styles.list}>
+            {[...Array(totalPages)].map((_, index) => {
+                return <button 
+                className={styles.pageNumber}
+                onClick={() => handlePageClick(index + 1)}
+                disabled={index + 1 === currentPage}
+                key={index}>
+                    {index + 1}
+                </button>
+            })}
+           </div>
+           <button
+            className={styles.arrow}
+            onClick={handleNextPage}
+            disabled={currentPage >= 10}
+            >{'>'}</button>
+        </div>
+    )
+}
+
+export default Pagination;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,31 +4,66 @@ import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import NewsList from "../../components/NewsList/NewsList";
 import Sceleton from "../../components/Sceleton/Sceleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const PageSize = 10;
+
+  const fetchNews = async (currentPage) => {
+    try {
+      setIsLoading(true)
+      const response = await getNews(currentPage, PageSize)
+      setNews(response.news)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try{
-        setIsLoading(true)
-        const response = await getNews()
-        setNews(response.news)
-        setIsLoading(false)
-    } catch (error){
-        console.log(error);
-    }
-    }
-    fetchNews();
-  }, [])
+    fetchNews(currentPage);
+  }, [currentPage])
 
-    return(
-        <main className={styles.main}>
-          {news.length > 0 && !isLoading ? <NewsBanner item={news[0]}/> : <Sceleton type={'banner'} count={1}/>}
-          {!isLoading ? <NewsList news={news}/> : <Sceleton type={'item'} count={10}/>}
-        </main>
-    )
+  const handleNextPage = () => {
+    if(currentPage < totalPages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  const handlePrevtPage = () => {
+    if(currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  const handlePageClick = (PageNumber) => {
+    setCurrentPage(PageNumber)
+  }
+
+  return (
+    <main className={styles.main}>
+      {news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Sceleton type={'banner'} count={1} />}
+      <Pagination 
+      totalPages={totalPages}
+      handleNextPage={handleNextPage} 
+      handlePrevtPage={handlePrevtPage}
+      handlePageClick={handlePageClick}
+      currentPage={currentPage}
+      />
+      {!isLoading ? <NewsList news={news} /> : <Sceleton type={'item'} count={10} />}
+      <Pagination 
+      totalPages={totalPages}
+      handleNextPage={handleNextPage} 
+      handlePrevtPage={handlePrevtPage}
+      handlePageClick={handlePageClick}
+      currentPage={currentPage}
+      />
+    </main>
+  )
 }
 
 export default Main;
